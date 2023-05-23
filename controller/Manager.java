499194f8cb2449a4424a263cf7a9effde55757db
FarimanS <farimansakaki2@gmail.com>
2023-05-23 14:30:36 +0430

    savelocal

--- controller/Manager.java
+++ controller/Manager.java
@@ -7,10 +7,28 @@ import view.Inputs;
 public class Manager {
     private Errors checkValidLogin (String userName , String pass , Inputs input) {
         Errors error = null ;
-        if (input == Inputs.ADD_CUSTOMER)
+        if (input == Inputs.ADD_CUSTOMER) {
+            Customer customer = Customer.getCustomerByUserName(userName);
+            if (customer == null)
+                return Errors.INVALID_USER_NAME;
+            if (!customer.pass.equals(pass))
+                return Errors.INVALID_PASSWORD;
+            Customer.
+        }
             error = Customer.checkValidLogin(userName,pass);
         //else if (input == Inputs.ADD_ADMIN)
         //    error = Admin.checkValidLogin(userName,pass);
         return error;
+    }
+    public static Errors checkValidLogin (String userName,String pass) {
+        Customer customer = Customer.getCustomerByUserName(userName);
+        if (customer == null)
+            return Errors.INVALID_USER_NAME;
+        if (!customer.pass.equals(pass))
+            return Errors.INVALID_PASSWORD;
+        else {
+            loggedInCustomer = customer;
+            return Errors.SUCCESSFUL_LOGIN;
+        }
     }
 }
--- model/Customer.java
+++ model/Customer.java
@@ -18,15 +18,5 @@ public class Customer extends User {
                 return customer;
         return null;
     }
-    public static Errors checkValidLogin (String userName,String pass) {
-        Customer customer = Customer.getCustomerByUserName(userName);
-        if (customer == null)
-            return Errors.INVALID_USER_NAME;
-        if (!customer.pass.equals(pass))
-            return Errors.INVALID_PASSWORD;
-        else {
-            loggedInCustomer = customer;
-            return Errors.SUCCESSFUL_LOGIN;
-        }
-    }
+
 }
