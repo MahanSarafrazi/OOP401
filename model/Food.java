64832b50e8facb56cc6e7f75508773c2c4e3e8f3
Reza <rezasaeedi.1383.com@gmail.com>
2023-05-23 18:11:18 +0330

    commit_1

--- model/Food.java
+++ model/Food.java
@@ -3,13 +3,15 @@ package model;
 public class Food {
     private double price;
     private String name;
+    private long ID;
     private double discount;
      public Food( String name, double price){
         this.name=name;
         this.price=price;
         this.discount = 0 ;
     }
-    
+    public void setDiscount(double discount){ this.discount=discount;}
+
 
 
 
--- model/Restaurant.java
+++ model/Restaurant.java
@@ -4,18 +4,34 @@ import java.util.ArrayList;
 
 public class Restaurant {
     //Location locate ;
+    //Edit Location;
      private String name;
+     public Restaurant(String name) {this.name=name;}
+
      public String getName(){return name;}
      private long ID;
      public long getID(){return ID;}
      private ArrayList<FoodType> foodTypes ;
      public void setFoodType(FoodType foodType){foodTypes.add(foodType);}
-     public void editFoodType(FoodType FirstfoodType,FoodType SecondaryFoodType){
+     public boolean editFoodType(FoodType FirstfoodType,FoodType SecondaryFoodType){
+         //*************************************** چک کردن سفارش موجود؟؟
          for (int i = 0; i < foodTypes.size(); i++) {
-             if (foodTypes.get(i) == FirstfoodType){foodTypes.set(i , SecondaryFoodType);}
+             if (foodTypes.get(i) == FirstfoodType){
+                 foodTypes.set(i , SecondaryFoodType);
+                 for (int j = foods.size()-1; j > 0; j--) {
+                     foods.remove(j);
+                 }
+                 return true;
+             }
          }
+         return false;
      }
+         //*************************************************************
+     public ArrayList<FoodType> ShowFoodType (){return foodTypes;}
      private ArrayList<Food> foods ;
+
+
+
 
 
 
