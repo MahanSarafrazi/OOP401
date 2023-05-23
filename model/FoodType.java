c1741d33865dd3f66ea6264726a83305b0854b29
FarimanS <farimansakaki2@gmail.com>
2023-05-24 00:45:15 +0430

    merge bug

--- model/FoodType.java
+++ model/FoodType.java
@@ -1,14 +1,4 @@
 package model;
 
 public enum FoodType {
-    IRANIAN_FOOD,
-    FAST_FOOD,
-    FRIED_FOOD,
-    SEA_FOOD,
-    INTERNATIONAL_FOOD,
-    DESSERT,
-    DRINK,
-    ;
-
-
 }
--- model/Restaurant.java
+++ model/Restaurant.java
@@ -3,24 +3,4 @@ package model;
 import java.util.ArrayList;
 
 public class Restaurant {
-    //Location locate ;
-     private String name;
-     public String getName(){return name;}
-     private long ID;
-     public long getID(){return ID;}
-     private ArrayList<FoodType> foodTypes ;
-     public void setFoodType(FoodType foodType){foodTypes.add(foodType);}
-     public void editFoodType(FoodType FirstfoodType,FoodType SecondaryFoodType){
-         for (int i = 0; i < foodTypes.size(); i++) {
-             if (foodTypes.get(i) == FirstfoodType){foodTypes.set(i , SecondaryFoodType);}
-         }
-     }
-     private ArrayList<Food> foods ;
-
-
-
-     //ArrayList<FoodType> RestaurantFoodType = new ArrayList<>();
-
-
-
 }
