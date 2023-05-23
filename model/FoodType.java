8cb9d2ec5cfc0de127f41879e2b594a0cb663756
Reza <rezasaeedi.1383.com@gmail.com>
2023-05-23 11:32:35 +0330

    not finished1

--- model/FoodType.java
+++ model/FoodType.java
@@ -1,5 +1,15 @@
 package model;
 
 public enum FoodType {
+    //ایرانی فست فود سوخاری دریایی بین الملل غذا سرد
+    IRANIAN_FOOD,
+    FAST_FOOD,
+    FRIED_FOOD,
+    SEA_FOOD,
+    INTERNATIONAL_FOOD,
+    DESSERT,
+    DRINK,
+    ;
+
 
 }
--- model/Restaurant.java
+++ model/Restaurant.java
@@ -1,4 +1,15 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Restaurant {
+    //Location locate ;
+     private String name;
+     private long ID;
+     ArrayList<FoodType> foodTypes ;
+
+     //ArrayList<FoodType> RestaurantFoodType = new ArrayList<>();
+
+
+
 }
