039ac3ab9d79316549ce0557fe67ecd9f1b505c0
Reza <rezasaeedi.1383.com@gmail.com>
2023-06-04 19:54:16 +0330

    Merge remote-tracking branch 'origin/Mahan' into reza
    # Conflicts:
    #	Main.java
    #	model/Food.java
    #	out/production/OOP401/Main.class
    #	out/production/OOP401/controller/DataBase.class
    #	out/production/OOP401/controller/Manager.class
    #	out/production/OOP401/model/Admin.class
    #	out/production/OOP401/model/Customer.class
    #	out/production/OOP401/model/Deliverer.class
    #	out/production/OOP401/model/Food.class
    #	out/production/OOP401/model/FoodType.class
    #	out/production/OOP401/model/Order.class
    #	out/production/OOP401/model/RatesAndComments.class
    #	out/production/OOP401/model/Restaurant.class
    #	out/production/OOP401/model/RestaurantOwner.class
    #	out/production/OOP401/model/User.class
    #	out/production/OOP401/view/Inputs.class
    #	out/production/OOP401/view/Output.class

--- model/Order.java
+++ model/Order.java
@@ -5,18 +5,45 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class Order {
-
-
-    private LinkedHashMap< Food, Integer> foods;
-
-    public LinkedHashMap<Food, Integer> getFoods() {
+    private final int ID;
+    public int getID(){ return ID;}
+     private double timeFinishing=0;
+    private ArrayList<Food> foods=new ArrayList<>();
+    public ArrayList<Food> getFoods(){
         return foods;
+
+    }
+    public Order(Food food){
+        foods.add(food);
+        RandomIDGenerator randomIDGenerator = new RandomIDGenerator();
+        this.ID=randomIDGenerator.getLastNumber();
     }
 
-    public void setFoods(LinkedHashMap<Food, Integer> foods) {
+    public ArrayList<FoodType> getType(){
+        ArrayList<FoodType> foodTypes=new ArrayList<>();
+        for (int i = 0; i < foods.size(); i++) {
+            foodTypes.add(foods.get(i).getType());
+        }
+        return foodTypes;
+    }
+
+    public void setFoods(ArrayList<Food> foods) {
         this.foods = foods;
     }
-
+    public  double BillWithoutDiscount(){
+        double price=0;
+        for (int i = 0; i < foods.size(); i++) {
+            price+=foods.get(i).getPrice();
+        }
+        return price;
+    }
+    public  double BillWithDiscount(){
+        double price=0;
+        for (int i = 0; i < foods.size(); i++) {
+            price+=foods.get(i).getDiscountedPrice();
+        }
+        return price;
+    }
 
 
 }
