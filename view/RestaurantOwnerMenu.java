08cdab0da213338ee6a6ac55bf6f579d6cfa8c19
mahan <mahansarafrazi.sut@gmail.com>
2023-06-06 10:09:04 +0330

    break

--- view/RestaurantOwnerMenu.java
+++ view/RestaurantOwnerMenu.java
@@ -85,7 +85,9 @@ public class RestaurantOwnerMenu extends Menu {
                 owner.getRestaurants().sort(Comparator.comparing(Restaurant::getName).thenComparing(Restaurant::getID));
                 for (Restaurant restaurant : owner.getRestaurants()) {
                     System.out.println(restaurant.getName()+" "+restaurant.getID());
-                }}
+                }
+                break;
+            }
             case NO_SUCH_FOOD_TYPE_IN_GENERAL -> System.out.println("There is no food type with this name!");}
     }
 
