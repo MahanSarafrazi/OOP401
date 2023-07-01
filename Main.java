9cca99b95b53f4219a9349165e317c5c517f4247
FarimanS <farimansakaki2@gmail.com>
2023-07-02 00:59:08 +0330

    idk

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import view.Run;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Shahkar");
+        System.out.println("SHAHKAR");
         Run runner = Run.getRun();
         runner.running();
     }
