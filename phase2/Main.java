7cb81699d041deb9437e8985f2b850c64b64beaa
mahan <mahansarafrazi.sut@gmail.com>
2023-07-02 16:58:19 +0330

    chert

--- phase2/Main.java
+++ phase2/Main.java
@@ -3,14 +3,17 @@ package phase2;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
     //this is for phase2
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.show();
+         primaryStage.setFullScreen(true);
+         primaryStage.show();
     }
 }
