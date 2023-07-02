3679a3a0db1f9059d435fb41144da207f7cc8bce
FarimanS <farimansakaki2@gmail.com>
2023-07-02 21:55:38 +0330

    debug v10.0

--- phase1/Main.java
+++ phase1/Main.java
@@ -4,7 +4,7 @@ import phase1.view.Run;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Shahkar");
+        System.out.println("Shahkaaaar");
         Run runner = Run.getRun();
         runner.running();
     }
