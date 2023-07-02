24f0b4787e099b155734061d89d78872a2c424b8
mahan <mahansarafrazi.sut@gmail.com>
2023-07-02 23:01:21 +0330

    progress2

--- phase2/controller/RegisterMenuController.java
+++ phase2/controller/RegisterMenuController.java
@@ -3,7 +3,10 @@ package phase2.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import phase2.model.UserType;
+import phase2.view.Output;
 
 public class RegisterMenuController extends MenuController {
 
@@ -22,7 +25,27 @@ public class RegisterMenuController extends MenuController {
     @FXML
     public Button back;
 
+    @FXML
+    public ChoiceBox type;
+
+    @FXML
     public void backHandler(ActionEvent actionEvent) {
         back();
+    }
+
+    @FXML
+    public void confirmHandler(ActionEvent actionEvent) {
+        String username = this.userName.getText();
+        String password = this.passWord.getText();
+        UserType userType = null;
+        if(type.getValue().equals("customer")) {
+            userType = UserType.CUSTOMER;
+        } else if(type.getValue().equals("restaurant owner")) {
+            userType = UserType.RESTAURANT_OWNER;
+        } else if(type.getValue().equals("deliverer")) {
+            userType = UserType.DELIVERER;
+        }
+        Output output = super.getManager().addUser(username, password, userType);
+        System.out.println(output);
     }
 }
