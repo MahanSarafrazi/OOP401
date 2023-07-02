52947433e7c1fba3d7a011d1a149aa746b402678
FarimanS <farimansakaki2@gmail.com>
2023-07-02 20:15:14 +0330

    lol

--- phase2/Main.java
+++ phase2/Main.java
@@ -1,12 +1,10 @@
 package phase2;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.*;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import phase2.controller.RegisterAndLoginMenuController;
-
-import java.awt.*;
 
 public class Main extends Application {
     //this is for phase2
