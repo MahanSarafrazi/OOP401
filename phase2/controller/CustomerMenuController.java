9e846fc6ee2f35e7356db169d6d05cdfe6daf4e2
FarimanS <farimansakaki2@gmail.com>
2023-07-06 15:54:31 +0330

    customer menu

--- phase2/controller/CustomerMenuController.java
+++ phase2/controller/CustomerMenuController.java
@@ -5,9 +5,24 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class CustomerMenuController extends MenuController {
+    @FXML
+    public ComboBox searchType;
+
+    @FXML
+    public ComboBox restaurantType;
+
+    @FXML
+    public TextField searchField;
+
+    @FXML
+    public void searchHandler() {
+        System.out.println("hi");
+    }
 }
 
