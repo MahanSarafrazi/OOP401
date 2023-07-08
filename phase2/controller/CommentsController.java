687bcd59c42ad4f6dfeb400818de4df94b37f6e3
mahan <mahansarafrazi.sut@gmail.com>
2023-07-08 23:29:34 +0330

    comment

--- phase2/controller/CommentsController.java
+++ phase2/controller/CommentsController.java
@@ -18,16 +18,30 @@ public class CommentsController extends MenuController implements Initializable 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        for (Comment comment : getManager().getLoggedInUser().getActiveRestaurant().getComments()) {
-            FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../view/Boxcomment.fxml"));
-            try {
-                loader.load();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        if(getManager().getLoggedInUser().getActiveRestaurant().getOpenedFood() == null) {
+            for (Comment comment : getManager().getLoggedInUser().getActiveRestaurant().getComments()) {
+                FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../view/Boxcomment.fxml"));
+                try {
+                    loader.load();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                commentsList.getChildren().add(loader.getRoot());
+                ((CommentsBoxController) loader.getController()).initialize(getStage(), getFatherStageController(), getMainScene(), null);
+                ((CommentsBoxController) loader.getController()).chooseComment(comment);
             }
-            commentsList.getChildren().add(loader.getRoot());
-            ((CommentsBoxController) loader.getController()).initialize(getStage(), getFatherStageController(), getMainScene(), null);
-            ((CommentsBoxController) loader.getController()).chooseComment(comment);
+        } else {
+            for (Comment comment : getManager().getLoggedInUser().getActiveRestaurant().getOpenedFood().getComments()) {
+                FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../view/Boxcomment.fxml"));
+                try {
+                    loader.load();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                commentsList.getChildren().add(loader.getRoot());
+                ((CommentsBoxController) loader.getController()).initialize(getStage(), getFatherStageController(), getMainScene(), null);
+                ((CommentsBoxController) loader.getController()).chooseComment(comment);
+            }
         }
     }
 }
