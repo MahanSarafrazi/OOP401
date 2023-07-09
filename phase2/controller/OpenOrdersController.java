f3ecf8883e064822317b5f626619fb8dee82ce5e
FarimanS <farimansakaki2@gmail.com>
2023-07-09 19:23:39 +0330

    Map uploaded

--- phase2/controller/OpenOrdersController.java
+++ phase2/controller/OpenOrdersController.java
@@ -7,8 +7,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 import phase2.model.*;
+import phase2.view.ZoomableScrollPane;
 
 import java.io.IOException;
 import java.net.URL;
@@ -134,5 +138,64 @@ public class OpenOrdersController extends MenuController {
     @FXML
     public void backHandler() {
         back();
+    }
+
+    @FXML
+    public void showPath() {
+        Stage primaryStage = new Stage();
+        AnchorPane pane = new AnchorPane();
+        int[][] coordinates = Manager.getManagerInstance().getMap().coordinates;
+        Manager manager = Manager.getManagerInstance();
+        int[][] adjacency = manager.getMap().getAdjacencyMatrix();
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < 1000; j++) {
+                if (adjacency[i][j] != 0) {
+                    Line line = new Line();
+                    line.setStartX(coordinates[i][0]);
+                    line.setEndX(coordinates[j][0]);
+                    line.setStartY(coordinates[i][1]);
+                    line.setEndY(coordinates[j][1]);
+                    pane.getChildren().add(line);
+                }
+            }
+        }
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < 1000; j++) {
+                if (adjacency[i][j] != 0) {
+                    Circle circle = new Circle(coordinates[i][0], coordinates[i][1], 25, Paint.valueOf("red"));
+                    pane.getChildren().add(circle);
+                }
+            }
+        }
+        manager.getMap().findShortestPath(order.getRestaurantLocation(), order.getCustomerLocation(), true);
+        for (int i = 1; i < manager.getMap().path.size(); i++) {
+            Line line = new Line();
+            line.setStroke(Paint.valueOf("green"));
+            line.setStrokeWidth(25);
+            line.setStartX(coordinates[manager.getMap().path.get(i - 1)][0]);
+            line.setEndX(coordinates[manager.getMap().path.get(i)][0]);
+            line.setStartY(coordinates[manager.getMap().path.get(i - 1)][1]);
+            line.setEndY(coordinates[manager.getMap().path.get(i)][1]);
+            if (i == 1) {
+                Circle circle = new Circle(line.getStartX(), line.getStartY(), 50, Paint.valueOf("orange"));
+                pane.getChildren().add(circle);
+            }
+            String color = "yellow";
+            int size = 25;
+            if (i == manager.getMap().path.size() - 1) {
+                color = "blue";
+                size = 50;
+            }
+            Circle circle = new Circle(line.getEndX(), line.getEndY(), size, Paint.valueOf(color));
+            pane.getChildren().add(line);
+            pane.getChildren().add(circle);
+        }
+        ZoomableScrollPane zoomableScrollPane = new ZoomableScrollPane(pane);
+        zoomableScrollPane.setMaxHeight(400);
+        zoomableScrollPane.setMaxWidth(600);
+        Scene scene = new Scene(zoomableScrollPane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Map");
+        primaryStage.show();
     }
 }
