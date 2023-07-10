c9846536ec4b2f5cf54f66a332bb6d35d6adc04f
mahan <mahansarafrazi.sut@gmail.com>
2023-07-10 18:41:57 +0330

    responsive

--- phase2/controller/OpenOrdersController.java
+++ phase2/controller/OpenOrdersController.java
@@ -77,7 +77,7 @@ public class OpenOrdersController extends MenuController {
             restaurantName.setText(order.getRestaurantName());
             restaurantName.setEditable(false);
         }
-        if(order.getOrderStatus().equals(OrderStatus.SENT)) {
+        if(order.getOrderStatus().equals(OrderStatus.ON_THE_WAY)) {
             orderStatus.setSelected(true);
             orderStatus.setDisable(true);
         }
@@ -223,7 +223,7 @@ public class OpenOrdersController extends MenuController {
 
     public void orderStatusHandler(ActionEvent actionEvent) {
         if(orderStatus.isSelected()) {
-            order.setOrderStatus(OrderStatus.SENT);
+            order.setOrderStatus(OrderStatus.ON_THE_WAY);
             orderStatus.setDisable(true);
         }
     }
